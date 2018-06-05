2370921bf850f1e4d461d13bf92ad32d4fa0be59
vitzhou <zsh989163@163.com>
2018-06-05 20:16:22 +0800

    http proxy poc

--- envoy.go
+++ envoy.go
@@ -3,23 +3,11 @@ package main
 import (
 	"net/http"
 	"log"
-	"net/http/httputil"
-	"net/url"
-	"math/rand"
+	"go-envoy-poc/proxy"
 )
-func NewMultipleHostsReverseProxy(targets []*url.URL) *httputil.ReverseProxy {
-	director := func(req *http.Request) {
-		target := targets[rand.Int()%len(targets)]
-		req.URL.Scheme = target.Scheme
-		req.URL.Host = target.Host
-		req.URL.Path = target.Path
-	}
-	return &httputil.ReverseProxy{Director: director}
-}
 
-
-func main(){
-	h := &handle{host: "127.0.0.1", port: "9955"}
+func main() {
+	h := &proxy.HttpProxy{DestHost: "127.0.0.1", DestPort: 9955}
 	err := http.ListenAndServe(":9966", h)
 	if err != nil {
 		log.Fatalln("ListenAndServe: ", err)
